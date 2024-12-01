eb64c3b77879774b75566b0ed16abafc95dd5e29
florCecotto24 <fcecotto@itba.edu.ar>
2024-12-01 19:57:33 -0300

    shadow

--- src/backend/model/Shadow.java
+++ src/backend/model/Shadow.java
@@ -1,5 +1,23 @@
 package backend.model;
 
-public enum Shadow {
 
+public enum Shadow {
+    SIMPLE(false, false),
+    COLORED(false, true),
+    SIMPLE_INVERSED(true, false),
+    COLORED_INVERSED(true, true);
+
+    final int offset;
+    final boolean isColored;
+    final int OFFSET = 10;
+
+    Shadow(boolean isInversed, boolean isColored){
+        this.offset = isInversed ? -1 * OFFSET : OFFSET;
+        this.isColored = isColored;
+    }
+
+    @Override
+    public String toString(){
+        return name();
+    }
 }
