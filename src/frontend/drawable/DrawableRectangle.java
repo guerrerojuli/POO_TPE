e3f8719ce62778afc83e43f40336480f9a07826f
Julian Guerrero <julian1guerrero@gmail.com>
2024-12-02 08:40:53 -0300

    supe que sombra va :D

--- src/frontend/drawable/DrawableRectangle.java
+++ src/frontend/drawable/DrawableRectangle.java
@@ -15,7 +15,7 @@ public class DrawableRectangle extends Rectangle implements Drawable {
 
     @Override
     public void drawShape(GraphicsContext gc) {
-        format.getShadow().drawShadow(gc, this, ); // NO SE QUE VA EN EL COLOR, atte: juli.
+        format.getShadow().drawShadow(gc, this, format.getFirstFillColor());
         gc.fillRect(this.getTopLeft().getX(), this.getTopLeft().getY(),
                 Math.abs(this.getTopLeft().getX() - this.getBottomRight().getX()), Math.abs(this.getTopLeft().getY() - this.getBottomRight().getY()));
         gc.strokeRect(this.getTopLeft().getX(), this.getTopLeft().getY(),
