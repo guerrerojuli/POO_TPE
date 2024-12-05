07d0bbd26730c270f6aba8defc6a9843dd96d9bf
Julian Guerrero <julian1guerrero@gmail.com>
2024-12-05 17:33:21 -0300

    merge Drawable and DrawableFigure

--- src/frontend/drawable/DrawableFigure.java
+++ src/frontend/drawable/DrawableFigure.java
@@ -3,21 +3,33 @@ package frontend.drawable;
 import backend.model.Figure;
 import frontend.format.Format;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public interface DrawableFigure extends Drawable, Figure{
+public interface DrawableFigure extends Figure {
     double OFFSET = 20;
-    void setFormat(Format format);
-    Format getFormat();
-
-    @Override
-    default void drawShadow(GraphicsContext gc){
-        getFormat().getShadow().drawShadow(gc, this, getFormat().getFirstFillColor());
-    }
 
     DrawableFigure duplicate();
     ArrayList<DrawableFigure> divide();
 
+    default void draw(GraphicsContext gc, boolean isSelected){
+        gc.setStroke(Color.TRANSPARENT);
+        drawShadow(gc);
+        drawGradient(gc);
+        drawBezel(gc);
+        gc.setStroke(isSelected ? Color.RED : Color.BLACK);
+        drawShape(gc);
+    }
+
+    void drawShape(GraphicsContext gc);
+    void drawGradient(GraphicsContext gc);
+    void drawBezel(GraphicsContext gc);
+    default void drawShadow(GraphicsContext gc){
+        getFormat().getShadow().drawShadow(gc, this, getFormat().getFirstFillColor());
+    }
+
+    void setFormat(Format format);
+    Format getFormat();
 }
